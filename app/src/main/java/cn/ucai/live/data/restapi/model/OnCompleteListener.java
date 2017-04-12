9a6e68a47841af5e81500fcd808ec1db207042ce
IKnowYouKnow <158241892@qq.com>
2017-04-12 15:08:09 +0800

    修改包名

---
+++ app/src/main/java/cn/ucai/live/data/restapi/model/OnCompleteListener.java
@@ -0,0 +1,11 @@
+package cn.ucai.live.data.restapi.model;
+
+
+import cn.ucai.live.utils.OkHttpUtils;
+
+/**
+ * Created by Administrator on 2017/3/29 0029.
+ */
+
+public interface OnCompleteListener<T> extends OkHttpUtils.OnCompleteListener<T> {
+}
