d1386dff50f33a9922e206af065cde5ba5f41953
IKnowYouKnow <158241892@qq.com>
2017-04-13 15:01:03 +0800

    初始化LiveHelper

--- app/src/main/java/cn/ucai/live/LiveApplication.java
+++ app/src/main/java/cn/ucai/live/LiveApplication.java
@@ -1,16 +1,10 @@
 package cn.ucai.live;
 
 import android.app.Application;
-import android.content.Intent;
 
-import com.hyphenate.EMConnectionListener;
-import com.hyphenate.EMError;
-import com.hyphenate.chat.EMClient;
-import com.hyphenate.easeui.controller.EaseUI;
 import com.ucloud.ulive.UStreamingContext;
 
-import cn.ucai.live.ui.activity.MainActivity;
-import cn.ucai.live.utils.PreferenceManager;
+import cn.ucai.live.data.LiveHelper;
 
 /**
  * Created by wei on 2016/5/27.
@@ -23,10 +17,7 @@ public class LiveApplication extends Application{
   @Override public void onCreate() {
     super.onCreate();
     instance = this;
-
-
-    initChatSdk();
-
+    LiveHelper.getInstance().init(this);
     //UEasyStreaming.initStreaming("publish3-key");
 
     UStreamingContext.init(getApplicationContext(), "publish3-key");
@@ -36,31 +27,5 @@ public class LiveApplication extends Application{
     return instance;
   }
 
-  private void initChatSdk(){
-    //EMOptions options = new EMOptions();
-    //options.enableDNSConfig(false);
-    //options.setRestServer("120.26.4.73:81");
-    //options.setIMServer("120.26.4.73");
-    //options.setImPort(6717);
-    PreferenceManager.init(this);
-    EaseUI.getInstance().init(this, null);
-    EMClient.getInstance().setDebugMode(true);
-
-    EMClient.getInstance().addConnectionListener(new EMConnectionListener() {
-      @Override public void onConnected() {
-
-      }
-
-      @Override public void onDisconnected(int errorCode) {
-        if(errorCode == EMError.USER_LOGIN_ANOTHER_DEVICE)
-        {
-          Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-          intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-          intent.putExtra("conflict", true);
-          startActivity(intent);
-        }
-      }
-    });
-  }
 
 }
